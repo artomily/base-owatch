bcd94b221a49de947e31299e35b70e6f082e44a8
artomily <vararakya@gmail.com>
2025-09-03 00:43:59 +0700

    fix import for deploy

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,4 @@
-export { BaseLayout } from './BaseLayout';
+export { default as BaseLayout } from './BaseLayout';
 export { Header } from './Header';
 export { MobileNav } from './MobileNav';
 export { Sidebar } from './Sidebar';
